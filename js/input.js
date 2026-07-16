7d6c06034308bc5b81efb84a9df1c847e0bb113f
Asteroid-84 <robolithic@gmail.com>
2026-07-16 12:47:54 -0700

    Update input.js

--- js/input.js
+++ js/input.js
@@ -1,9 +1,33 @@
 const keys = {};
+const pressedKeys = {};
 
 window.addEventListener("keydown", function(event){
+
+    if (!keys[event.key]) {
+        pressedKeys[event.key] = true;
+    }
+
     keys[event.key] = true;
+
 });
 
+
 window.addEventListener("keyup", function(event){
+
     keys[event.key] = false;
+
 });
+
+
+function consumeKey(key){
+
+    if (pressedKeys[key]){
+
+        pressedKeys[key] = false;
+        return true;
+
+    }
+
+    return false;
+
+}
