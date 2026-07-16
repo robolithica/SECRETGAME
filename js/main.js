d6acae281bb39d0c17caf0395b66f7c8986d6f8e
Asteroid-84 <robolithic@gmail.com>
2026-07-16 11:17:12 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -12,8 +12,8 @@ ctx.fillRect(0,0,canvas.width,canvas.height);
 // Draw test text
 ctx.fillStyle = "white";
 ctx.font = "16px monospace";
-ctx.fillText("Retro RPG Engine",50,40);
+ctx.fillText("Gospel of Decay",50,40);
 
 // Draw a player placeholder
-ctx.fillStyle = "red";
+ctx.fillStyle = "blue";
 ctx.fillRect(150,130,16,16);
